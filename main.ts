6fc9de7e67a53fb7e67d77d7ecb7b858e33d7562
João Victor Dias Gonçalves <109822096+victordg21@users.noreply.github.com>
2023-02-15 18:28:03 -0300

    Arquivo TypeScript

---
+++ main.ts
@@ -0,0 +1,10 @@
+function multiplicacao(primeiroValor: number, segundoValor: number){
+    return (primeiroValor * segundoValor)
+}
+
+function dizNome(nome: string){
+    return (`Olá ${nome}`)
+}
+
+console.log(multiplicacao(9,5))
+console.log(dizNome("MAteus"))
